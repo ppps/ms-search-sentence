c9a7687f36e87ba8a6409817ed005b9e39b7214c
Rob Wells <rob@robjwells.com>
2017-12-10 21:48:48 +0000

    Add standfirst and closing functions to JS

--- editor.js
+++ editor.js
@@ -18,11 +18,22 @@ function setTitle(text) {
     return document.querySelector('#edit-title').value = text;
 }
 
+function setStandfirst(text) {
+    return document.querySelector('#edit-field-lead-und-0-value').value = text;
+}
+
+function codePointsJSON(text) {
+    return JSON.stringify(
+        Array.from(text).map(c => c.codePointAt(0))
+    );
+}
+
 function setReadyForPublishing() {
     // 4 is the index of the `Ready for Publishing` state
     var el = document.querySelector('#edit-workbench-moderation-state-new');
     return el.selectedIndex = 4;
 }
 
-// one-liner for applescript
-// CKEDITOR.instances['edit-body-und-0-value'].getData().split('</p>', 1)[0].slice(3);
+function saveAndClose() {
+    document.querySelector('#edit-submit-1').click();
+}
